091659232dbbb10988d7db2d532b347d167dc8b8
Jared Jahnke <77992735+IJaredI@users.noreply.github.com>
2022-10-23 23:59:16 -0600

    Home page max vp rough done

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@
 
 let imageContainer = document.getElementById('mainBackgroundPic');
 
-window.addEventListener("scroll", function () {
-    let offset = window.scrollY
-    imageContainer.style.backgroundPositionY = offset * -1.2 + "px";
-})
+// window.addEventListener("scroll", function () {
+//     let offset = window.scrollY
+//     imageContainer.style.backgroundPositionY = offset * -1.2 + "px";
+// })
